f91f9b3f694678fa531cae65fa9bafe3a4945eac
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-12 22:26:18 +0000

    Checking for existence of user and authentication

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, currentUser, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      currentUser && currentUser.isAuthenticated ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: '/login',
-            state: { from: props.location },
-          }}
-        />
-      )
-    }
-  />
-);
+const PrivateRoute = ({ component: Component, currentUser, ...rest }) => {
+  return(
+    <Route
+      {...rest}
+      render={(props) =>
+        currentUser && currentUser.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default PrivateRoute;
