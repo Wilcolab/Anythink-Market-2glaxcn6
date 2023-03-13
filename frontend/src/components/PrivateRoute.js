bd2e9e48674b04c4fd8ede467169b9c69a183216
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-13 00:41:57 +0000

    Reverted back to original code

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-function PrivateRoute({ currentUser, children }) {
-	return (
-		<Route 
-      render={() => (currentUser ? 
-        children : 
-        <Redirect to=
-        "/login" />)} />
-    );
-}
-
-// const PrivateRoute = ({ component: Component, currentUser, ...rest }) => {
-//   return(
-//     <Route
-//       {...rest}
-//       render={(props) =>
-//         currentUser ? (
-//           <Component {...props} 
-//           /> 
-//           ) : (
-//           <Redirect
-//             to=
-//             '/login'
-//           />
-//         )
-//       }
-//     />
-//   );
-// };
+const PrivateRoute = ({ component: Component, currentUser, ...rest }) => {
+  return(
+    <Route
+      {...rest}
+      render={(props) =>
+        currentUser ? (
+          <Component {...props} 
+          /> 
+          ) : (
+          <Redirect
+            to=
+            '/login'
+          />
+        )
+      }
+    />
+  );
+};
 
 export default PrivateRoute;
