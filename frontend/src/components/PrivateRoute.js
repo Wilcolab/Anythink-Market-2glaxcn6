3d7943fa675f0cd3945fffa6e54e5127cd8c6077
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-13 00:25:36 +0000

    Commented out original PrivateRoute component

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, currentUser, ...rest }) => {
-  return(
-    <Route
-      {...rest}
-      render={(props) =>
-        currentUser ? (
-          <Component {...props} />
-          
-        ) : (
-          <Redirect
-            to=
-            '/login'
-          />
-        )
-      }
-    />
-  );
-};
+function PrivateRoute({ currentUser, children }) {
+	return (
+		<Route 
+      render={() => (currentUser ? 
+        children : 
+        <Redirect to=
+        "/login" />)} />
+    );
+}
+
+// const PrivateRoute = ({ component: Component, currentUser, ...rest }) => {
+//   return(
+//     <Route
+//       {...rest}
+//       render={(props) =>
+//         currentUser ? (
+//           <Component {...props} 
+//           /> 
+//           ) : (
+//           <Redirect
+//             to=
+//             '/login'
+//           />
+//         )
+//       }
+//     />
+//   );
+// };
 
 export default PrivateRoute;
