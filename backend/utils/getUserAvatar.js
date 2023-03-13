e7cdf1273a8e8829125af265c26ab6304f4a9546
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-13 23:32:00 +0000

    Moved createCanvas inside function

--- backend/utils/getUserAvatar.js
+++ backend/utils/getUserAvatar.js
@@ -1,6 +1,5 @@
-const { createCanvas } = require('canvas') 
-
 function getUserAvatar(user) {
+    const { createCanvas } = require('canvas') 
     const canvas = createCanvas(200,200);
     const ctx = canvas.getContext("2d");
     
@@ -8,7 +7,7 @@ function getUserAvatar(user) {
     ctx.fillRect(0,0, canvas.width, canvas.height);
 
     ctx.fillStyle = '#fff';
-    ctx.font = 'bold 100px Arial';
+    ctx.font = '100px Bradley Hand';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.fillText(user.username.substring(0,2).toUpperCase(), canvas.width/2, canvas.height/2);
