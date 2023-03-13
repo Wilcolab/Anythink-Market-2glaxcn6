57ed8bca350e8a3df086529f3ed194efde03b033
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-13 23:01:22 +0000

    Built module for randomly generating an avatar

---
+++ backend/utils/getUserAvatar.js
@@ -0,0 +1,19 @@
+const { createCanvas } = require('canvas') 
+
+function getUserAvatar(user) {
+    const canvas = createCanvas(200,200);
+    const ctx = canvas.getContext("2d");
+    
+    ctx.fillStyle = `#${Math.floor(Math.random() * 16777215).to_String(16)}`;
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    ctx.fillStyle = '#fff';
+    ctx.font = '100px Bradley Hand';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(user.username.substring(0,2).toUpperCase(), canvas.width/2, canvas.height/2);
+
+    return canvas.toDataURL();
+}
+
+module.exports = getUserAvatar;
