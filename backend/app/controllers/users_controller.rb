b709a789d7cf1e6d8da8c15ead920ecd2a5a6382
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-15 01:03:11 +0000

    Modified UsersController to save avatarUrl

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -12,10 +12,10 @@ class UsersController < ApplicationController
       render json: { errors: current_user.errors }, status: :unprocessable_entity
     end
   end
-
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:username, :email, :password, :bio, :image)
   end
-end
+end  
