a490d5d2823d274006652b6d483ad69dc995934e
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-15 02:23:53 +0000

    Open policy CORS

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
     devise_for :users, controllers: { sessions: :sessions, registrations: :registrations },
                        path_names: { sign_in: :login }
    
-    resource :user, only: %i[show update]
+    resource :user, only: %i[show update] do
+      put 'image', to: 'users#update'
+    end
+
 
     resources :profiles, param: :username, only: [:show] do
       resource :follow, only: %i[create destroy]
--- backend/utils/saveAvatar.js
+++ backend/utils/saveAvatar.js
@@ -3,13 +3,18 @@ import getUserAvatar from './getUserAvatar';
 const user = { username: 'example' };
 const avatarUrl = getUserAvatar(user);
 
+const authToken = localStorage.getItem('authToken');
+
 const requestOptions = {
   method: 'PUT',
-  headers: { 'Content-Type': 'application/json' },
+  headers: { 
+    'Content-Type': 'application/json', 
+    'Authorization': `Bearer ${authToken}` 
+  },
   body: JSON.stringify({ user: { image: avatarUrl, ...user } })
 };
 
-fetch('/users', requestOptions)
+fetch('/api/users/image', requestOptions)
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.log(error));
