cf748928d94fa4f91acf30dc73075e0c8e59d015
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-23 02:51:24 +0000

    Built a search bar with a handleTitleSearch()

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,9 +1,9 @@
-import Banner from "./Banner";
-import MainView from "./MainView";
 import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import Banner from "./Banner";
+import ItemList from "./../ItemList";
 import Tags from "./Tags";
 import agent from "../../agent";
-import { connect } from "react-redux";
 import {
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
@@ -26,29 +26,64 @@ const mapDispatchToProps = (dispatch) => ({
   onUnload: () => dispatch({ type: HOME_PAGE_UNLOADED }),
 });
 
-const Home = ({onLoad, onUnload, tags, onClickTag}) => {
-  const tab = "all";
-  const itemsPromise = agent.Items.all;
-
+const Home = ({
+  onLoad,
+  onUnload,
+  tags,
+  onClickTag,
+  items,
+  tagFilter,
+  onTitleSearch,
+}) => {
   useEffect(() => {
+    const itemsPromise = agent.Items.all({ title: "" });
     onLoad(
-      tab,
+      "all",
       itemsPromise,
-      Promise.all([agent.Tags.getAll(), itemsPromise()])
+      Promise.all([agent.Tags.getAll(), itemsPromise])
     );
     return onUnload;
-  }, [onLoad, onUnload, tab, itemsPromise]);
+  }, [onLoad, onUnload]);
 
-    return (
-      <div className="home-page">
-        <Banner />
+  const handleTitleSearch = (e) => {
+    e.preventDefault();
+    onTitleSearch(e.target.elements.searchBox.value);
+  };
 
-        <div className="container page">
-          <Tags tags={tags} onClickTag={onClickTag} />
-          <MainView />
+  return (
+    <div className="home-page">
+      <Banner />
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            <ItemList items={items} />
+          </div>
+          <div className="col-md-3">
+            <div className="sidebar">
+              <p>Popular Tags</p>
+              <Tags tags={tags} tagFilter={tagFilter} onClickTag={onClickTag} />
+              <hr />
+              <div className="search">
+                <p>Search Articles</p>
+                <form onSubmit={handleTitleSearch}>
+                  <input
+                    type="text"
+                    name="searchBox"
+                    id="search-box"
+                    className="form-control"
+                    placeholder="Search articles"
+                  />
+                  <button className="btn btn-outline-secondary mt-3">
+                    Search
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
