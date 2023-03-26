f12ec0fe876d0cddb3a6d083e3fed8cbbf7e0c8e
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-03-26 17:55:40 +0000

    Built handleSearch function, imported SEARCH_TITLE

--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -6,6 +6,7 @@ import {
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
   CHANGE_TAB,
+  SEARCH_TITLE,
   PROFILE_PAGE_LOADED,
   PROFILE_PAGE_UNLOADED,
   PROFILE_FAVORITES_PAGE_LOADED,
@@ -58,6 +59,17 @@ const reducer = (state = {}, action) => {
       };
     case HOME_PAGE_UNLOADED:
       return {};
+    case SEARCH_TITLE:
+      return {
+        ...state,
+        pager: action.pager,
+        searchValue: action.searchValue,
+        items: action.payload.items,
+        itemsCount: action.payload.itemsCount,
+        tab: action.tab,
+        currentPage: 0,
+        tag: null,
+      };
     case CHANGE_TAB:
       return {
         ...state,
