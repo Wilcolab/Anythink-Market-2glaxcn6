4c93cb71ce9e5e9b8dec232fe0aabaf7ebbe1f37
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-04-21 19:21:37 +0000

    Added attribute, isVerified, to User model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -18,6 +18,8 @@ class User < ApplicationRecord
                        presence: true,
                        allow_blank: false
 
+  attribute :isVerified, :boolean, default: false
+
   def generate_jwt
     JWT.encode({ id: id,
                  exp: 60.days.from_now.to_i },
