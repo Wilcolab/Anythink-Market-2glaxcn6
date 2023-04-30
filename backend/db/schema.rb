099818ad12f6074dd30274ea0fc5164441f6f81d
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-04-30 22:26:26 +0000

    updated schema with isVerified boolean

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_190028) do
+ActiveRecord::Schema.define(version: 2023_04_30_222033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
